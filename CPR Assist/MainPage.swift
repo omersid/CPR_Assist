ea948fd29defaf9d1bb9e249f575375e8aee1a61
Omer Siddiqui <omersid@umich.edu>
2020-03-16 18:51:08 -0400

    Breaths control

--- CPR Assist/MainPage.swift
+++ CPR Assist/MainPage.swift
@@ -12,15 +12,21 @@ import AVFoundation
 class MainPage: UIViewController {
     //MARK: Settings
     var counter = 0
-    var timer = Timer()
-    var player: AVAudioPlayer?
-    var player2: AVAudioPlayer?
+    var beatTimer = Timer()
+    var beatPlayer: AVAudioPlayer?
+    var beatPlayer2: AVAudioPlayer?
     
     @IBOutlet weak var settingsButton: UIBarButtonItem!
     @IBOutlet weak var compressions: UILabel!
     @IBOutlet weak var heartpic: UIImageView!
     
-    func setBeatTimer() {
+    @IBOutlet weak var breathText: UILabel!
+    @IBOutlet weak var breathNoiseButton: UISwitch!
+    @IBOutlet weak var advancedAirwayButton: UISwitch!
+    var breathTimer = Timer()
+    var breathPlayer: AVAudioPlayer?
+    
+    func setBeatPlayer() {
         guard let url = Bundle.main.url(forResource: "beep", withExtension: "wav") else { return }
         
         do {
@@ -28,8 +34,8 @@ class MainPage: UIViewController {
             try AVAudioSession.sharedInstance().setActive(true)
 
             /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            player2 = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            beatPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            beatPlayer2 = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
 
             /* iOS 10 and earlier require the following line:
             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
@@ -37,23 +43,24 @@ class MainPage: UIViewController {
             print(error.localizedDescription)
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.settingsButton.title = NSString(string: "\u{2699}\u{0000FE0E}") as String
-        if let font = UIFont(name: "Helvetica", size: 24.0){ self.settingsButton.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+    func setBreathPlayer() {
+        guard let url = Bundle.main.url(forResource: "breath", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            breathPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+        } catch let error {
+            print(error.localizedDescription)
         }
-        compressions.text = "Compressions \(counter)"
-        let interval: Double = 60/100
-        setBeatTimer()
-        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block: makeNoise)
     }
-    
     func makeNoise(t: Timer) {
         counter += 1
         compressions.text = "Compressions: \(counter)"
         if counter%2 != 0{
-            player!.play()
+            beatPlayer!.play()
             UIView.animate(withDuration: 0.6, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
                 self.heartpic.transform = CGAffineTransform.identity.scaledBy(x: 1.3, y: 1.3) //
                 }) { (finished) in
@@ -62,8 +69,50 @@ class MainPage: UIViewController {
                     })
             }
         }else{
-            player2!.play()
+            beatPlayer2!.play()
         }
         
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set default button behavior:
+        breathNoiseButton.isOn = false
+        advancedAirwayButton.isOn = false
+        
+        // Set settings button icon:
+        self.settingsButton.title = NSString(string: "\u{2699}\u{0000FE0E}") as String
+        if let font = UIFont(name: "Helvetica", size: 24.0){ self.settingsButton.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+        }
+        
+        //Set up players:
+        setBeatPlayer()
+        setBreathPlayer()
+        
+        //Get compressions going:
+        compressions.text = "Compressions \(counter)"
+        let interval: Double = 60/100
+        beatTimer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block: makeNoise)
+    }
+    
+    //MARK: Actions
+    @IBAction func advancedIn(_ sender: UISwitch) {
+        if sender.isOn{
+            breathNoiseButton.isHidden = false
+            breathText.text = "1 Breath every 6 Seconds"
+        }else{
+            breathNoiseButton.isHidden = true
+            breathText.text = "2 Breaths every 30 Compressions"
+        }
+    }
+    @IBAction func breathNoise(_ sender: UISwitch) {
+        if sender.isOn{
+            breathTimer = Timer.scheduledTimer(withTimeInterval: 6, repeats: true, block: { timer in
+                self.breathPlayer!.play()
+            })
+        }else{
+            breathTimer.invalidate()
+        }
     }
 }
