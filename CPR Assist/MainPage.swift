4ee23f35f0bc372423e766e31b216b0b39bc5d90
Omer Siddiqui <omersid@umich.edu>
2020-03-15 22:12:09 -0400

    Starting project, few screens done

---
+++ CPR Assist/MainPage.swift
@@ -0,0 +1,19 @@
+//
+//  MainPage.swift
+//  CPR Assist
+//
+//  Created by Omer Siddiqui on 3/15/20.
+//  Copyright © 2020 Omer Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class MainPage: UIViewController {
+    //MARK: Settings
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //MARK: Actions
+}
---
+++ CPR Assist/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CPR Assist
+//
+//  Created by Omer Siddiqui on 3/15/20.
+//  Copyright © 2020 Omer Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var tutorial: UIButton!
+    @IBOutlet weak var settings: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //MARK: Actions
+    @IBAction func clickTutorial(_ sender: UIButton) {
+    }
+}
